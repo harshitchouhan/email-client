dd86d008e6e901c76c5e4c31d86baaf61220c3c1
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-12-23 09:22:31 +0530

    signin and signout functionality added

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
 import { AuthService } from '../auth.service';
@@ -7,7 +8,7 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css'],
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
   authForm = new FormGroup(
@@ -18,20 +19,20 @@ export class SignupComponent implements OnInit {
           Validators.required,
           Validators.minLength(3),
           Validators.maxLength(20),
-          Validators.pattern(/^[a-z0-9]+$/),
+          Validators.pattern(/^[a-z0-9]+$/)
         ],
         [this.uniqueUsername.validate]
       ),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
-        Validators.maxLength(20),
+        Validators.maxLength(20)
       ]),
       passwordConfirmation: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
-        Validators.maxLength(20),
-      ]),
+        Validators.maxLength(20)
+      ])
     },
     { validators: [this.matchPassword.validate] }
   );
@@ -39,7 +40,8 @@ export class SignupComponent implements OnInit {
   constructor(
     private matchPassword: MatchPassword,
     private uniqueUsername: UniqueUsername,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -50,16 +52,16 @@ export class SignupComponent implements OnInit {
     }
 
     this.authService.signup(this.authForm.value).subscribe({
-      next: (response) => {
-        // Navigate to some other route
+      next: response => {
+        this.router.navigateByUrl('/inbox');
       },
-      error: (err) => {
+      error: err => {
         if (!err.status) {
           this.authForm.setErrors({ noConnection: true });
         } else {
           this.authForm.setErrors({ unknownError: true });
         }
-      },
+      }
     });
   }
 }
